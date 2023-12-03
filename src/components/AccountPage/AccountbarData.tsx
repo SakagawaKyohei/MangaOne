1d5db8084413e8a1ba8e49dd435975f48a8c2f97
sakagawakyohei <lamgiabao1039@gmail.com>
2023-12-03 17:11:10 +0700

    optimize account bar

--- src/components/AccountPage/AccountbarData.tsx
+++ src/components/AccountPage/AccountbarData.tsx
@@ -1,53 +1,44 @@
 import path from "path";
 import React from "react";
-import { FaInfoCircle } from "react-icons/fa";
-import { PiBookFill } from "react-icons/pi";
-import { FaBookmark } from "react-icons/fa";
-import { FaCoins } from "react-icons/fa6";
-import { FaLock } from "react-icons/fa6";
-import { RiLogoutBoxRFill } from "react-icons/ri";
+import * as FaIcons from "react-icons/fa";
+const style: React.CSSProperties = {
+  marginRight: 15,
+  fontSize: 18,
+};
 export const AccountbarData = [
   {
     num: 1,
     title: "Thông tin tài khoản",
-    icon: <FaInfoCircle style={{ marginRight: 15, fontSize: 18 }} />,
+    icon: <FaIcons.FaInfoCircle style={style} />,
     path: "/thong-tin-tai-khoan",
   },
   {
     num: 2,
     title: "Truyện theo dõi",
-    icon: <FaBookmark style={{ marginRight: 15, fontSize: 18 }} />,
+    icon: <FaIcons.FaBookmark style={style} />,
     path: "/thong-tin-tai-khoan",
   },
   {
     num: 3,
     title: "Truyện đã đăng",
-    icon: <PiBookFill style={{ marginRight: 15, fontSize: 18 }} />,
+    icon: <FaIcons.FaBook style={style} />,
     path: "/thong-tin-tai-khoan",
   },
   {
     num: 4,
     title: "Điểm dịch truyện",
-    icon: <FaCoins style={{ marginRight: 15, fontSize: 18 }} />,
+    icon: <FaIcons.FaCoins style={style} />,
     path: "/thong-tin-tai-khoan",
   },
   {
     num: 5,
     title: "Đổi mật khẩu",
-    icon: <FaLock style={{ marginRight: 15, fontSize: 18 }} />,
+    icon: <FaIcons.FaLock style={style} />,
     path: "/thong-tin-tai-khoan",
   },
   {
     title: "Đăng xuất",
-    icon: (
-      <RiLogoutBoxRFill
-        style={{
-          fontSize: 20,
-          marginRight: 15,
-          marginBottom: -3,
-        }}
-      />
-    ),
+    icon: <FaIcons.FaSignOutAlt style={style} />,
     path: "/thong-tin-tai-khoan",
   },
 ];
